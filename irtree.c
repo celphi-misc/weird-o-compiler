615c2e4d9434ac8102769a64128455420131b558
ZephyrL <3150104024@zju.edu.cn>
2018-06-10 12:19:33 +0800

    finish debugging irtree generation and printing

--- irtree.c
+++ irtree.c
@@ -319,7 +319,7 @@ TreeNode IRAutoLabel(char* name, char** newName){
     this->pos = -1;
     this->numOfChild = 1;
     this->childs = newNodeList(1);
-    (this->childs)[0] = IRLeafName(name);
+    (this->childs)[0] = IRLeafName(*newName);
     return this;
 }
 
@@ -954,15 +954,14 @@ char* newAutoLabel(char* name){
 #ifdef _DEBUG
     fprintf(stdout, "in newAutoLabel\n");
 #endif
-    name = appendName(name, currentScope->id);
-
+    char* ret = appendName(name, currentScope->id);
     Label newL = (Label)malloc(sizeof(*newL));
     newL->name = (char*)malloc(strlen(name)+1);
-    strcpy(newL->name, name);
+    strcpy(newL->name, ret);
     newL->type = AUTO;
     newL->next = currentScope->autoLabelList;
     currentScope->autoLabelList = newL;
-    return name;
+    return ret;
 }
 
 void newVar(char* name){
@@ -1126,11 +1125,11 @@ char buffer[BUF_SIZE];
 int printJson(TreeNode node, char* buf){
     char* oldBuf = buf;
     if(node->numOfChild == 0){
-        buf+=sprintf(buf, "\"name\": \"%s\"", node->name);
+        buf+=sprintf(buf, "{\"node\": \"%s\"", node->name);
     } else if(node->numOfChild > 0){
-        buf+=sprintf(buf, "\"node\":\"%s\"", node->name);
+        buf+=sprintf(buf, "{\"node\":\"%s\"", node->name);
         for(int i = 0; i< node->numOfChild; i++){
-            buf+= sprintf(buf, ",\"%d\": {", i);
+            buf+= sprintf(buf, ",\"%d\":\n", i);
             buf += printJson((node->childs)[i], buf);
         }
     }
--- parser.y
+++ parser.y
@@ -356,11 +356,11 @@ int main(int argc, char **argv)
         yyin = stdin;
     yyparse();
     char *json;
-    if(ASTroot)
-    {
-        json = createAstJsonStr(ASTroot);
-        printf(json);
-    }
+    // if(ASTroot)
+    // {
+    //     json = createAstJsonStr(ASTroot);
+    //     printf(json);
+    // }
     IRroot = IRTree(ASTroot);
     char* jsonIR;
     jsonIR = createIRJsonStr(IRroot);
